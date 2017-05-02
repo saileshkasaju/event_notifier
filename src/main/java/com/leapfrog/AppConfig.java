70a50fe1b8215f9694a879576bdf58dba75b3e7c
Sailesh Kasaju <leosailesh@gmail.com>
2017-05-02 17:29:00 +0545

    Use web.xml to configure dispatcher servlet

--- src/main/java/com/leapfrog/AppConfig.java
+++ src/main/java/com/leapfrog/AppConfig.java
@@ -1,20 +1,22 @@
 package com.leapfrog;
 
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+//import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by Edge on 4/28/2017.
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan({"com.leapfrog"})
-@PropertySource("app.properties")
+//@PropertySource("app.properties")
 public class AppConfig {
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
 }
---
+++ src/main/java/com/leapfrog/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.leapfrog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sailesh on 5/2/17.
+ */
+
+@Controller
+public class HelloController {
+
+  @RequestMapping(value = "/greeting")
+  public String sayHello(Model model) {
+    model.addAttribute("greeting", "Hello World");
+    return "hello.jsp";
+  }
+}
