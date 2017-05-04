f9fd32104c0ecaa78d7793f15db84e8675091734
saileshkasaju <leosailesh@gmail.com>
2017-05-04 18:30:49 +0545

    add a mapping to handle event post

--- src/main/java/com/leapfrog/controller/EventController.java
+++ src/main/java/com/leapfrog/controller/EventController.java
@@ -3,6 +3,7 @@ package com.leapfrog.controller;
 import com.leapfrog.model.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,6 +17,12 @@ public class EventController {
         Event event = new Event();
         event.setName("Java user group");
         model.addAttribute("event", event);
+        return "event";
+    }
+
+    @RequestMapping(value = "/event", method = RequestMethod.POST)
+    public String processEvent(@ModelAttribute("event") Event event) {
+        System.out.println(event);
         return "event";
     }
 }
