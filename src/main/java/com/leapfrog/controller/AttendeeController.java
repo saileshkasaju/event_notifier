accf38941a737c62bcded930f8ccea72f8c2b065
saileshkasaju <leosailesh@gmail.com>
2017-05-12 19:19:07 +0545

    clean code

--- src/main/java/com/leapfrog/controller/AttendeeController.java
+++ src/main/java/com/leapfrog/controller/AttendeeController.java
@@ -4,11 +4,8 @@ import com.leapfrog.model.Attendee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
 import javax.validation.Valid;
 
 /**
@@ -16,6 +13,10 @@ import javax.validation.Valid;
  */
 @Controller
 public class AttendeeController {
+
+//    @Autowired
+//    private AttendeeService attendeeService;
+
 
     @RequestMapping(value = "/attendee", method = RequestMethod.GET)
     public String displayAttendeePage(Model model) {
