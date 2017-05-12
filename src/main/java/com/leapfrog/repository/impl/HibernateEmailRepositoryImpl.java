c39ccaa28406b767b3985470a63e567cc2bf3e0a
saileshkasaju <leosailesh@gmail.com>
2017-05-12 19:14:47 +0545

    comment out hibernate Style for now, first finish the implementation in Spring JDBC

--- src/main/java/com/leapfrog/repository/impl/HibernateEmailRepositoryImpl.java
+++ src/main/java/com/leapfrog/repository/impl/HibernateEmailRepositoryImpl.java
@@ -1,32 +1,32 @@
-package com.leapfrog.repository.impl;
-
-import com.leapfrog.model.Email;
-import com.leapfrog.repository.EmailRepository;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Edge on 4/28/2017.
- */
-@Repository("emailRepository")
-public class HibernateEmailRepositoryImpl implements EmailRepository {
-
-    @Value("${dbUsername}")
-    private String dbUsername;
-
-    @Override
-    public List<Email> findAll() {
-        System.out.println(dbUsername);
-        List<Email> emails = new ArrayList<>();
-        Email email = new Email();
-
-        email.setId(1);
-        email.setEmail("leosailesh@gmail.com");
-
-        emails.add(email);
-        return emails;
-    }
-}
+//package com.leapfrog.repository.impl;
+//
+//import com.leapfrog.model.Email;
+//import com.leapfrog.repository.EmailRepository;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Created by Edge on 4/28/2017.
+// */
+////@Repository("emailRepository")
+//public class HibernateEmailRepositoryImpl implements EmailRepository {
+//
+//    @Value("${dbUsername}")
+//    private String dbUsername;
+//
+//    @Override
+//    public List<Email> findAll() {
+//        System.out.println(dbUsername);
+//        List<Email> emails = new ArrayList<>();
+//        Email email = new Email();
+//
+//        email.setId(1);
+//        email.setEmail("leosailesh@gmail.com");
+//
+//        emails.add(email);
+//        return emails;
+//    }
+//}
--- src/main/java/com/leapfrog/repository/impl/HibernateEventRegisterRepositoryImpl.java
+++ src/main/java/com/leapfrog/repository/impl/HibernateEventRegisterRepositoryImpl.java
@@ -1,27 +1,27 @@
-package com.leapfrog.repository.impl;
-
-import com.leapfrog.model.EventRegister;
-import com.leapfrog.repository.EventRegisterRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Edge on 4/28/2017.
- */
-@Repository("eventRegisterRepository")
-public class HibernateEventRegisterRepositoryImpl implements EventRegisterRepository {
-
-    @Override
-    public List<EventRegister> findAll() {
-        List<EventRegister> eventRegisters = new ArrayList<>();
-        EventRegister eventRegister = new EventRegister();
-
-        eventRegister.setId(1);
-
-        eventRegisters.add(eventRegister);
-
-        return eventRegisters;
-    }
-}
+//package com.leapfrog.repository.impl;
+//
+//import com.leapfrog.model.EventRegister;
+//import com.leapfrog.repository.EventRegisterRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Created by Edge on 4/28/2017.
+// */
+////@Repository("eventRegisterRepository")
+//public class HibernateEventRegisterRepositoryImpl implements EventRegisterRepository {
+//
+//    @Override
+//    public List<EventRegister> findAll() {
+//        List<EventRegister> eventRegisters = new ArrayList<>();
+//        EventRegister eventRegister = new EventRegister();
+//
+//        eventRegister.setId(1);
+//
+//        eventRegisters.add(eventRegister);
+//
+//        return eventRegisters;
+//    }
+//}
