5a9844815be146c92d38a4c281f9eed71f464ff1
saileshkasaju <leosailesh@gmail.com>
2017-05-12 19:18:08 +0545

    post api to create Event and get api to list all events created

--- src/main/java/com/leapfrog/controller/EventController.java
+++ src/main/java/com/leapfrog/controller/EventController.java
@@ -1,30 +1,50 @@
 package com.leapfrog.controller;
 
 import com.leapfrog.model.Event;
+import com.leapfrog.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Edge on 5/4/2017.
  */
-@Controller
+//@Controller
+@RestController
 @SessionAttributes("event")
 public class EventController {
-    @RequestMapping(value = "/event", method = RequestMethod.GET)
-    public String displayEventPage(Model model) {
-        Event event = new Event();
-        event.setTitle("Java user group");
-        model.addAttribute("event", event);
-        return "event";
+
+    private EventService eventService;
+
+    @Autowired
+    public void setEventService(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @RequestMapping(value = "/events", method = RequestMethod.GET)
+    public List<Event> getEvents() {
+        return eventService.findAll();
     }
 
     @RequestMapping(value = "/event", method = RequestMethod.POST)
-    public String processEvent(@ModelAttribute("event") Event event) {
-        System.out.println(event);
-        return "redirect:index.html";
+    public Event createEvent(@RequestBody Event event) {
+        eventService.createEvent(event);
+        return null;
     }
+//    @RequestMapping(value = "/event", method = RequestMethod.GET)
+//    public String displayEventPage(Model model) {
+//        Event event = new Event();
+//        event.setTitle("Java user group");
+//        model.addAttribute("event", event);
+//        return "event";
+//    }
+//
+//    @RequestMapping(value = "/event", method = RequestMethod.POST)
+//    public String processEvent(@ModelAttribute("event") Event event) {
+//        System.out.println(event);
+//        return "redirect:index.html";
+//    }
 }
