8e6a2348bb2b0d01bdba5ffbf64375805c489f5a
saileshkasaju <leosailesh@gmail.com>
2017-05-12 19:16:31 +0545

    basic Repository implementations

--- src/main/java/com/leapfrog/repository/impl/EmailRepositoryImpl.java
+++ src/main/java/com/leapfrog/repository/impl/EmailRepositoryImpl.java
@@ -1,7 +1,28 @@
 package com.leapfrog.repository.impl;
 
+import com.leapfrog.model.Email;
+import com.leapfrog.model.Event;
+import com.leapfrog.repository.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Edge on 5/10/2017.
  */
-public class EmailRepositoryImpl {
+@Repository("emailRepository")
+public class EmailRepositoryImpl implements EmailRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Override
+    public List<Email> findAll() {
+        List<Email> emails = new ArrayList<>();
+        Email email = new Email();
+        email.setEmail("test@teset.com");
+        emails.add(email);
+        return emails;
+    }
 }
--- src/main/java/com/leapfrog/repository/impl/EventRegisterRepositoryImpl.java
+++ src/main/java/com/leapfrog/repository/impl/EventRegisterRepositoryImpl.java
@@ -1,7 +1,28 @@
 package com.leapfrog.repository.impl;
 
+import com.leapfrog.model.Event;
+import com.leapfrog.model.EventRegister;
+import com.leapfrog.repository.EventRegisterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Edge on 5/10/2017.
  */
-public class EventRegisterRepositoryImpl {
+@Repository("eventRegisterRepository")
+public class EventRegisterRepositoryImpl implements EventRegisterRepository{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Override
+    public List<EventRegister> findAll() {
+        List<EventRegister> eventRegisters = new ArrayList<>();
+        EventRegister event = new EventRegister();
+        event.setId(1);
+        eventRegisters.add(event);
+        return eventRegisters;
+    }
 }
